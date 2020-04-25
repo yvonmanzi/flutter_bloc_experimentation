4bd73346aabcdb80b7c9ae33e5af24b83238d7f6
Yvon <ym2637@columbia.edu>
2020-04-25 14:54:52 +0300

    the app is now done. i used a few libraries such as wave, but def need to revisit the app to modularize it.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timer/bloc/bloc.dart';
 import 'package:flutter_timer/ticker.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,33 +37,42 @@ class Timer extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter Timer'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 100.0),
-              child: Center(
-                child: BlocBuilder<TimerBloc, TimerState>(
-                  builder: (context, state) {
-                    final String minutesStr = ((state.duration / 60) % 60)
-                        .floor()
-                        .toString()
-                        .padLeft(2, '0');
-                    final String secondsStr = (state.duration % 60)
-                        .floor()
-                        .toString()
-                        .padLeft(2, '0');
-                    return Text(
-                      '$minutesStr: $secondsStr',
-                      style: Timer.timerTextStyle,
-                    );
-                  },
+        body: Stack(children: [
+          BackGround(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(
+                  child: BlocBuilder<TimerBloc, TimerState>(
+                    builder: (context, state) {
+                      final String minutesStr = ((state.duration / 60) % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      final String secondsStr = (state.duration % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      return Text(
+                        '$minutesStr: $secondsStr',
+                        style: Timer.timerTextStyle,
+                      );
+                    },
+                  ),
                 ),
               ),
-            )
-          ],
-        ));
+              BlocBuilder<TimerBloc, TimerState>(
+                // this is a new feature. the condition takes either true or false.
+                condition: (previousState, state) =>
+                    state.runtimeType != previousState.runtimeType,
+                builder: (context, state) => Actions(),
+              )
+            ],
+          ),
+        ]));
   }
 }
 
@@ -119,5 +130,39 @@ class Actions extends StatelessWidget {
       ];
     }
     return [];
+  }
+}
+
+class BackGround extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WaveWidget(
+      config: CustomConfig(
+        gradients: [
+          [
+            Color.fromRGBO(72, 74, 126, 1),
+            Color.fromRGBO(125, 170, 206, 1),
+            Color.fromRGBO(184, 189, 245, 0.7)
+          ],
+          [
+            Color.fromRGBO(72, 74, 126, 1),
+            Color.fromRGBO(125, 170, 206, 1),
+            Color.fromRGBO(172, 182, 219, 0.7)
+          ],
+          [
+            Color.fromRGBO(72, 73, 126, 1),
+            Color.fromRGBO(125, 170, 206, 1),
+            Color.fromRGBO(190, 238, 246, 0.7)
+          ],
+        ],
+        durations: [19440, 10800, 6000],
+        heightPercentages: [0.03, 0.01, 0.02],
+        gradientBegin: Alignment.bottomCenter,
+        gradientEnd: Alignment.topCenter,
+      ),
+      size: Size(double.infinity, double.infinity),
+      waveAmplitude: 25,
+      backgroundColor: Colors.blue[50],
+    );
   }
 }
