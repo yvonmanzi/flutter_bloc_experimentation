3e3b1dbd1e3e79068b1589bda310b529b2151d9a
Yvon <ym2637@columbia.edu>
2020-04-25 14:12:20 +0300

    just added BlocBuilder to build the ui every time the states changes. next is adding those actions that actually change the ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/bloc/bloc.dart';
+import 'package:flutter_timer/ticker.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(109, 234, 255, 1),
+        accentColor: Color.fromRGBO(72, 74, 126, 1),
+        brightness: Brightness.dark,
+      ),
+      title: 'Flutter Demo',
+      home: BlocProvider(
+        create: (context) => TimerBloc(ticker: Ticker()),
+        child: Timer(),
+      ),
+    );
+  }
+}
+
+class Timer extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Timer'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 100.0),
+              child: Center(
+                child: BlocBuilder<TimerBloc, TimerState>(
+                  builder: (context, state) {
+                    final String minutesStr = ((state.duration / 60) % 60)
+                        .floor()
+                        .toString()
+                        .padLeft(2, '0');
+                    final String secondsStr = (state.duration % 60)
+                        .floor()
+                        .toString()
+                        .padLeft(2, '0');
+                    return Text(
+                      '$minutesStr: $secondsStr',
+                      style: Timer.timerTextStyle,
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
