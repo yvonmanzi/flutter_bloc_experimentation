a87886e0cbccd16bff854dac4efefbdd95e226f8
Yvon <ym2637@columbia.edu>
2020-04-27 15:57:51 +0300

    implemented a repo and a bloc for our user login app. next is now drawing the interface.

---
+++ lib/bloc/authentication_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/repo/user_repo.dart';
+
+import 'authentication_event.dart';
+import 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepo;
+
+  AuthenticationBloc({@required this.userRepo})
+
+  @override
+  get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event) async* {
+    if (event is StartApp) {
+      final bool hasToken = await userRepo.hasToken();
+
+      if (hasToken)
+        yield AuthenticationAuthenticated();
+      else
+        yield AuthenticationUnAuthenticated();
+    }
+
+    if (event is LogIn) {
+      yield AuthenticationLoading();
+      await userRepo.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+    if (event is LogOut) {
+      yield AuthenticationLoading();
+      await userRepo.deleteToken();
+      yield AuthenticationUnAuthenticated();
+    }
+  }
+}
---
+++ lib/bloc/authentication_event.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class StartApp extends AuthenticationEvent {}
+
+class LogIn extends AuthenticationEvent {
+  final String token;
+
+  const LogIn({@required this.token});
+  @override
+  List<Object> get props => [token];
+
+  @override
+  String toString() {
+    return 'LogedIn { token: $token}';
+  }
+}
+
+class LogOut extends AuthenticationEvent {}
