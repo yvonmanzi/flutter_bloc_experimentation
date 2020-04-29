b489e995d4e47db8150ef62def418b2f849dbe28
Yvon <ym2637@columbia.edu>
2020-04-29 15:11:37 +0300

    created the repository. the entrance point of communication with the Bloc am about to create. it is actually not that complicated.

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+
+import '../models/item_model.dart';
+import 'movie_api_provider.dart';
+
+class Repository {
+  final moviesApiProvider = MovieApiProvider();
+  Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+}
