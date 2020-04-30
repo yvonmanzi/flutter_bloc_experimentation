a98df809640b2d5b136d1eeb4b0851f18fa4cd27
Yvon <ym2637@columbia.edu>
2020-04-30 20:12:52 +0300

    have created 2 blocks now. one is for app authentication. the other one is just for LoginForm. this is so cool.

---
+++ lib/src/bloc/login_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_login/src/bloc/authentication_event.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/src/bloc/authentication_bloc.dart';
+import 'package:flutter_login/src/bloc/login_events.dart';
+import 'package:flutter_login/src/bloc/login_state.dart';
+import 'package:flutter_login/src/repository/user_repo.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({@required this.userRepository, @required this.authenticationBloc})
+      :
+        assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+            username: event.username, password: event.password);
+
+        authenticationBloc.add(LogIn(token: token));
+        yield LoginInitial();
+      } catch(error){
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
